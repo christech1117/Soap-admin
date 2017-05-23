99b8b53a0ffd5c09cf3ecaafaad10a79566232ba
christech1117 <christech1117@gmail.com>
2017-05-23 18:01:07 +0800

    SideBar

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,26 +1,105 @@
 <template>
-<div class="block">
-  <span class="demonstration">默认显示颜色</span>
-  <span class="wrapper">
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-  </span>
-</div>
+    <div class="sidebar">
+        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router>
+            <template v-for="item in items">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index">
+                        <template slot="title"><i class="el-icon-menu"></i>{{ item.title }}</template>
+                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">{{ subItem.title }}
+                        </el-menu-item>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.index">
+                        <i class="el-icon-setting"></i>{{ item.title }}
+                    </el-menu-item>
+                </template>
+            </template>
+        </el-menu>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
   data () {
     return {
+      items: [
+        {
+          index: 'readme',
+          title: '自述'
+        },
+        {
+          index: '2',
+          title: '表格',
+          subs: [
+            {
+              index: 'basetable',
+              title: '基础表格'
+            },
+            {
+              index: 'vuetable',
+              title: 'Vue表格组件'
+            }
+          ]
+        },
+        {
+          index: '3',
+          title: '表单',
+          subs: [
+            {
+              index: 'baseform',
+              title: '基本表单'
+            },
+            {
+              index: 'vueeditor',
+              title: '编辑器'
+            },
+            {
+              index: 'markdown',
+              title: 'markdown'
+            },
+            {
+              index: 'upload',
+              title: '文件上传'
+            }
+          ]
+        },
+        {
+          index: '4',
+          title: '图表',
+          subs: [
+            {
+              index: 'basecharts',
+              title: '基础图表'
+            },
+            {
+              index: 'mixcharts',
+              title: '混合图表'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  computed: {
+    onRoutes () {
+      return this.$route.path.replace('/', '')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.sidebar{
+    display: block;
+    position: absolute;
+    width: 250px;
+    left: 0;
+    top: 70px;
+    bottom:0;
+    background: #2E363F;
+}
+.sidebar > ul {
+    height:100%;
+}
 </style>
